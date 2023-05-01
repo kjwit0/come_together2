17adf723388fd57667677998f811fc2e90f3ffcf
kjwit <puffern12@gmail.com>
2023-05-01 16:30:07 +0900

    first commit

---
+++ lib/pages/first_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
